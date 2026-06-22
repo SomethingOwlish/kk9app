60aee0d0f4acad6796eba6757bd621fc35abd1d7
SomethingOwlish <55540709+SomethingOwlish@users.noreply.github.com>
2026-06-22 23:58:09 +0200

    namespace

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'kk9App/',   // например '/kk9/' — со слешами с обеих сторон!
+  base: 'kk9app/',   // например '/kk9/' — со слешами с обеих сторон!
   plugins: [react()],
 })
