163082e0ffacd6977a196494fbd1c68a63df5c04
SomethingOwlish <55540709+SomethingOwlish@users.noreply.github.com>
2026-06-22 23:56:16 +0200

    reponame

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/ИМЯ-РЕПОЗИТОРИЯ/',   // например '/kk9/' — со слешами с обеих сторон!
+  base: 'kk9App/',   // например '/kk9/' — со слешами с обеих сторон!
   plugins: [react()],
 })
